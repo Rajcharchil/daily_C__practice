1f2f3744c7c2f443ff80e777505d6d78ccc9e648
charchil raj <114642639+Rajcharchil@users.noreply.github.com>
2023-02-05 11:24:46 +0530

    Add files via upload

--- 1st_day.c
+++ 1st_day.c
@@ -1,251 +1,253 @@
 # include<stdio.h>
 
-//int main(){
-//	printf("jai hanumaan  ");
-//	return 0;
-//}
+int main(){
+	printf("jai hanumaan  ");
+	return 0;
+}
 
 // ======================================================================================================
 #include<stdio.h>
-// int main()
-//  double first, second, temp;
-//  printf("Enter first number: ");
-//  scanf("%lf", &first);
-//  printf("Enter second number: ");
-//  scanf("%lf", &second);
+int main()
+ double first, second, temp;
+ printf("Enter first number: ");
+ scanf("%lf", &first);
+ printf("Enter second number: ");
+ scanf("%lf", &second);
 //
 //  // value of first is assigned to temp
-//  temp = first;
+ temp = first;
 //
 //  // value of second is assigned to first
-//  first = second;
+ first = second;
 //
 //  // value of temp (initial value of first) is assigned to second
-//  second = temp;
+ second = temp;
 //
 //  // %.2lf displays number up to 2 decimal points
-//  printf("\nAfter swapping, first number = %.2lf\n", first);
-//  printf("After swapping, second number = %.2lf", second);
-//  return 0;
-//}
+ printf("\nAfter swapping, first number = %.2lf\n", first);
+ printf("After swapping, second number = %.2lf", second);
+// 
 
 // ======================================================================================
 
-//int main(){
-////	int a = 4*3/6*2;
-////	printf("%d \n",a);
-////	int age = 13;
-////	printf("age is %d",age);
-////    int age;
-////    printf("enter age :");
-////    scanf("%d", &age);
-////    printf("age is: %d", age);
-//    float side;
-//    printf("enter a number:");
-//    scanf("%f",&side);
-//
-//	printf("%f",side*side);
-//
-//	return 0;
-//}
+int main(){
+	int a = 4*3/6*2;
+	printf("%d \n",a);
+	int age = 13;
+	printf("age is %d",age);
+   int age;
+   printf("enter age :");
+   scanf("%d", &age);
+   printf("age is: %d", age);
+   float side;
+   printf("enter a number:");
+   scanf("%f",&side);
+
+	printf("%f",side*side);
+
+	return 0;
+}
 
 // ==============================================================================
 #include<stdio.h>
 #include<math.h>
 
 int main() {
-	// float radius;
-	// printf("enter radius: ");
-	// scanf("%f",&radius);
-	// printf("%f",3.14*radius*radius);
+	float radius;
+	printf("enter radius: ");
+	scanf("%f",&radius);
+	printf("%f",3.14*radius*radius);
 
 
 
-//    int power = pow(b,c);
-//    printf("%d",power);
+   int power = pow(b,c);
+   printf("%d",power);
 
 
-//    int a= (int)9.99999;
-//    printf("%d ",a);
+   int a= (int)9.99999;
+   printf("%d ",a);
      
 
 
 // =============================================================================================
 
- //   printf("%d \n" ,4==4);
-//    printf("%d",4!=4);
-//    printf("%d \n",4>4);
-//    printf("%d",4<4);
-//    printf("%d \n",4==4);
-//    printf("%d \n", !(5>1));
-//    
-//    printf("logical condition \n");
-//    printf("%d \n", !((5>1) && (3>4)));
-//    printf("%d \n", 2*4+23-32+23*23+34-43);
+   printf("%d \n" ,4==4);
+   printf("%d",4!=4);
+   printf("%d \n",4>4);
+   printf("%d",4<4);
+   printf("%d \n",4==4);
+   printf("%d \n", !(5>1));
+   
+   printf("logical condition \n");
+   printf("%d \n", !((5>1) && (3>4)));
+   printf("%d \n", 2*4+23-32+23*23+34-43);
 
 
 // ======================================================================================
 
 //write a program to check  if a no is divisible by 2 or not?
 
-//    int x;
-//    printf("enter a no,:");
-//    scanf("%d",&x);
-//    printf("%d",x%2==0);
+   int x;
+   printf("enter a no,:");
+   scanf("%d",&x);
+   printf("%d",x%2==0);
     
-//    char stars = '*';
-//    printf("%c",stars);
+   char stars = '*';
+   printf("%c",stars);
       
 	  
-//	int ismonday = 0;
-//	int issunday = 1;
-//	printf("%d",ismonday || issunday);  
+	int ismonday = 0;
+	int issunday = 1;
+	printf("%d",ismonday || issunday);  
     
-//	int x;
-//	printf("enter no.:");
-//	scanf("%d",&x);
-//	printf("%d", x>9 && x<100); 
+	int x;
+	printf("enter no.:");
+	scanf("%d",&x);
+	printf("%d", x>9 && x<100); 
 
 
 // =================================================================================
 
 //write a average of three no.
    
-//    int a,b,c;
-//    float avg;
-//    printf("enter first no.:");
-//    scanf("%d",&a);
-//    printf("enter second no.:");
-//    scanf("%d",&b);
-//    printf("enter third no.:");
-//    scanf("%d",&c);
-//    avg=a+b+c/3.0;
-//    printf("average of three number:%.2f",avg);
+   int a,b,c;
+   float avg;
+   printf("enter first no.:");
+   scanf("%d",&a);
+   printf("enter second no.:");
+   scanf("%d",&b);
+   printf("enter third no.:");
+   scanf("%d",&c);
+   avg=a+b+c/3.0;
+   printf("average of three number:%.2f",avg);
 
     //===========================================================================   
       
-//    int a,b,c;
-//    float avg;
-//    printf("\tEnter Three Numbers\n");
-//    printf("--------------------------------\n");
-//    printf("Enter First Number  : ");
-//    scanf("%d", &a);
-//    
-//    printf("\nEnter Second Number : ");
-//    scanf("%d",&b);
-//    
-//    printf("\nEnter Third Number : ");
-//    scanf("%d",&c);
-//    printf("--------------------------------\n");
-//    /* To find average*/
-//    avg=a+b+c/3.0;
-//    printf("\n Average of Three Numbers : %f",avg);
+   int a,b,c;
+   float avg;
+   printf("\tEnter Three Numbers\n");
+   printf("--------------------------------\n");
+   printf("Enter First Number  : ");
+   scanf("%d", &a);
+   
+   printf("\nEnter Second Number : ");
+   scanf("%d",&b);
+   
+   printf("\nEnter Third Number : ");
+   scanf("%d",&c);
+   printf("--------------------------------\n");
+   /* To find average*/
+   avg=a+b+c/3.0;
+   printf("\n Average of Three Numbers : %f",avg);
 
 
 // ===================================================================================================
 
 // write a to program to check if given character is digit or not.
 
-//    char ch;
-//
-//   printf("Please enter a character: ");
-//   scanf("%c", &ch);
-//   if (isdigit(ch))
-//    printf("Given Character %c is Digit.", ch);
-//   else
-//    printf("\n Given Character %c is Not Digit.", ch);
+   char ch;
+
+  printf("Please enter a character: ");
+  scanf("%c", &ch);
+  if (isdigit(ch))
+   printf("Given Character %c is Digit.", ch);
+  else
+   printf("\n Given Character %c is Not Digit.", ch);
 ////  =============================================================================  
 
 
 //write a program to print the smallest number
      
     
-//    int num1,num2;	
-//    printf(" enter number 1:");
-//    scanf("%d",&num1);
-//    printf("enter number 2:");
-//    scanf("%d",&num2);
-//    printf("smallest numer:%d",num2>num1 ? num1:num2);
+   int num1,num2;	
+   printf(" enter number 1:");
+   scanf("%d",&num1);
+   printf("enter number 2:");
+   scanf("%d",&num2);
+   printf("smallest numer:%d",num2>num1 ? num1:num2);
 
 
 //====================================================================
-    // int age;
-    // printf("enter a number:");
-    // scanf("%d",&age);
+    int age;
+    printf("enter a number:");
+    scanf("%d",&age);
     
-    // if(age>18){
-    // 	printf("adult \n");
-    // 	printf("they can vote\n");
-    // 	printf("they  can drive");
+    if(age>18){
+    	printf("adult \n");
+    	printf("they can vote\n");
+    	printf("they  can drive");
     	
-	// }
-	// else{
-	// 	printf("not adult");
-	// }
+	}
+	else{
+		printf("not adult");
+	}
 
 	// ===========================================================================
 
-	// int marks;
-    // printf("enter a number:");
-    // scanf("%d",&marks);
+	int marks;
+    printf("enter a number:");
+    scanf("%d",&marks);
     
-    // if(marks <= 30){
-    //     printf("fail");
+    if(marks <= 30){
+        printf("fail");
 
-    // }else{
-    //     printf("pass");
-    // }
+    }else{
+        printf("pass");
+    }
 
 
 
 	// ==================================================================================
-	// int marks;
-    // printf("enter a number:");
-    // scanf("%d",&marks);
+	int marks;
+    printf("enter a number:");
+    scanf("%d",&marks);
 
-	// if (marks <30){
-	// 	printf("C \n");
-	// }
-	// else if (marks >= 30 && marks < 70){
-	// 	printf("B \n");
-	// }
-	// else if (marks >= 70 && marks < 100){
-	// 	printf("A \n");
-	// }
-	// else{
-	// 	printf("topper");
-	// }
+	if (marks <30){
+		printf("C \n");
+	}
+	else if (marks >= 30 && marks < 70){
+		printf("B \n");
+	}
+	else if (marks >= 70 && marks < 100){
+		printf("A \n");
+	}
+	else{
+		printf("topper");
+	}
 
 	// =====================================================================
-	// int a=2;
-	// if (a=1){             //==
-	// 	printf("x is equal to 1");
-	// }
-	// else{
-	// 	printf("x is not equal to 1");
-	// }
+	int a=2;
+	if (a=1){             //==
+		printf("x is equal to 1");
+	}
+	else{
+		printf("x is not equal to 1");
+	}
 
 	// ===========================================================================
 
-	// char ch;
-	// printf("enter a characater: ");
-	// scanf("%c", &ch);
+	char ch;
+	printf("enter a characater: ");
+	scanf("%c", &ch);
 
-	// if( ch >= 'a' && ch<= 'z'){
-	// 	printf("lower case");
-	// }
-	// else if (ch >= 'A' && ch <= 'Z'){
-	// 	printf("upper case");
-	// }
-	// else{
-	// 	printf("not english letter");
-	// }
+	if( ch >= 'a' && ch<= 'z'){
+		printf("lower case");
+	}
+	else if (ch >= 'A' && ch <= 'Z'){
+		printf("upper case");
+	}
+	else{
+		printf("not english letter");
+	}
 
 
 // ==================================================================================================
-    // for(char i=1; i<=10; i+=1){
-	// 	printf("charchil \n");
-	// }
+
+//for loop
+
+    for(char i=1; i<=10; i+=1){
+		printf("charchil \n");
+	}
 	for(float i=1.0; i<=5.0; i++) {
 		printf("%f \n",i);
 	}	
